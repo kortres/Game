f554cc8f769500f3a92c2025e360c2c91f2f59b6
Lazzytchik <GitLazzytchik@users.noreply.github.com>
2020-06-29 23:17:13 +0300

    Room builder edit

--- ProjectAlpha/ProjectAlpha/Assets/Scripts/RoomGenerator/RoomGenerator.cs
+++ ProjectAlpha/ProjectAlpha/Assets/Scripts/RoomGenerator/RoomGenerator.cs
@@ -6,5 +6,29 @@ public class RoomGenerator : MonoBehaviour
 {
     private int[,] map;
 
+    public void Intialize()
+    {
 
+    }
+
+    private void BuildRoom(int startIndex,int xMin, int xMax, int yMin, int yMax)
+    {
+        //for (int i = yMin; i < yMax; i++)
+        //{
+        //    for (int j = xMin; j < xMax; j++)
+        //    {
+        //        map[i, j] = 1;
+        //    }
+        //}
+        for (int i = xMin; i < xMax; i++)
+        {
+
+        }
+    }
+
+
+    private void BuildSpawn()
+    {
+
+    }
 }
